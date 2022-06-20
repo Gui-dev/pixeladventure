f2c0f545a522a9e1ecb03f4b61f46d9b94f6ed2b
Gui-dev <adrianon10@gmail.com>
2022-06-20 01:20:49 -0300

    chore: add first commit

---
+++ scripts/Player.gd
@@ -0,0 +1,13 @@
+extends KinematicBody2D
+
+
+var velocity = Vector2.ZERO
+var move_speed = 480
+var gravity = 1200
+var jump_force = -720
+
+func _ready() -> void:
+  pass
+  
+func _physics_process(delta: float) -> void:
+  pass
