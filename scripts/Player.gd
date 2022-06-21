4833dbb41a60c5200acf8aaeeb1b14b8505c8d70
Gui-dev <adrianon10@gmail.com>
2022-06-21 02:41:56 -0300

    feat: add animation. jump, run and hit

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -5,9 +5,45 @@ var velocity = Vector2.ZERO
 var move_speed = 480
 var gravity = 1200
 var jump_force = -720
+var is_grounded
+onready var raycasts = $raycasts 
 
 func _ready() -> void:
   pass
   
 func _physics_process(delta: float) -> void:
-  pass
+  velocity.y += gravity * delta
+  
+  _get_input()    
+  velocity = move_and_slide(velocity)
+  is_grounded = _check_is_grounded()
+  _set_animation()
+
+func _get_input() -> void:
+  velocity.x = 0
+  var move_direction = int(Input.is_action_pressed('ui_move_right')) - int(Input.is_action_pressed('ui_move_left'))
+  velocity.x = lerp(velocity.x,  move_speed * move_direction, 0.2)
+  
+  if move_direction != 0:
+    $texture.scale.x = move_direction
+
+func _input(event: InputEvent) -> void:
+  if event.is_action_pressed('ui_jump') and is_grounded:
+    velocity.y = jump_force / 2
+
+func _check_is_grounded() -> bool:
+  for raycast in raycasts.get_children():
+    if raycast.is_colliding():
+      return true
+  return false
+
+func _set_animation() -> void:
+  var animation = 'idle'
+  
+  if !is_grounded:
+    animation = 'jump'
+  elif velocity.x != 0:
+    animation = 'run'
+  
+  if $animation.assigned_animation != animation:
+    $animation.play(animation)
