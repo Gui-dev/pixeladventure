adb34fbe81ab0828a0501f9bb3f24a89a76da619
Gui-dev <adrianon10@gmail.com>
2022-06-26 04:27:32 -0300

    feat: add damage when hit by player

--- scripts/Mushroom.gd
+++ scripts/Mushroom.gd
@@ -3,21 +3,36 @@ extends KinematicBody2D
 
 export var speed = 64
 export var health = 1
-var velocitty = Vector2.ZERO
+var motion = Vector2.ZERO
 var move_direction = -1
+var gravity = 1200
+var hitted = false
 
 func _physics_process(delta: float) -> void:
-  velocitty.x = speed * move_direction
-  velocitty = move_and_slide(velocitty)
+  motion.x = speed * move_direction
+  motion.y += gravity * delta
   
   if move_direction == 1:
     $texture.flip_h = true
   else:
      $texture.flip_h = false
     
-  if $ray_wall.is_colliding():
-    $animation.play('idle')
+  _set_animation()    
+  motion = move_and_slide(motion)
 
+func _set_animation() -> void:
+  var animation = 'run'
+  
+  if $ray_wall.is_colliding():
+    animation = 'idle'
+  elif motion.x != 0:
+    animation = 'run'
+    
+  if hitted:
+    animation = 'hit'
+  
+  if $animation.assigned_animation != animation:
+    $animation.play(animation)
 
 func _on_animation_animation_finished(anim_name: String) -> void:
   if anim_name == 'idle':
@@ -25,3 +40,15 @@ func _on_animation_animation_finished(anim_name: String) -> void:
     $ray_wall.scale.x *= -1
     move_direction *= -1
     $animation.play('run')
+
+
+func _on_Hitbox_body_entered(body: Node) -> void:
+  hitted = true
+  health -= 1    
+  body.velocity.y -= 300 
+  yield(get_tree().create_timer(0.2), 'timeout')
+  hitted = false
+  
+  if health < 1:
+    queue_free()
+    get_node('Hitbox/collision').set_deferred('disabled', true)
