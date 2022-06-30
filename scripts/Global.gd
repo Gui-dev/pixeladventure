891b3942438c759a42bc77c8f5e6445dc72f7afe
Gui-dev <adrianon10@gmail.com>
2022-06-30 02:45:06 -0300

    feat: add checkpoint_position variable

--- scripts/Global.gd
+++ scripts/Global.gd
@@ -2,6 +2,7 @@ extends Node
 
 
 var fruits = 0
+var checkpoint_position = 0
 
 func _ready() -> void:
   pass
--- scripts/Reset_Level.gd
+++ scripts/Reset_Level.gd
@@ -1,5 +1,7 @@
 extends Node
 
 
+var checkpoint_position = 0
+
 func _ready() -> void:
   Global.fruits = 0
