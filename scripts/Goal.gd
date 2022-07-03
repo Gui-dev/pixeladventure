f638b7298fe31afaa68d1ecaaf0576a33f187538
Gui-dev <adrianon10@gmail.com>
2022-07-03 04:30:00 -0300

    feat: add new property Path

--- scripts/Goal.gd
+++ scripts/Goal.gd
@@ -1,6 +1,9 @@
 extends Area2D
 
 
+onready var changer = get_parent().get_node('Transition_in')
+export(String) var path
+
 func _ready() -> void:
   pass
 
@@ -8,3 +11,5 @@ func _ready() -> void:
 func _on_Goal_body_entered(body: Node) -> void:
   if body.name == 'Player':
     $confetti.emitting = true
+    changer.change_scene(path)
+    Global.checkpoint_position = 0
