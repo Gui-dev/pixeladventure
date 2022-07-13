67e5fc26656d46eefc418df51ff23d4902752be6
Gui-dev <adrianon10@gmail.com>
2022-07-13 03:09:44 -0300

    feat: add prefabs Trampoline

---
+++ scripts/Trampoline.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+
+func _ready() -> void:
+  pass
+
+
+func _on_Trampoline_body_entered(body: Node) -> void:
+  print(body)
+  body.velocity.y = (body.jump_force * 1.6) / 2
+  $animation.play('jump')
