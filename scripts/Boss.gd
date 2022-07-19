9920ee0bf607e930eb78ca85d1304c31c454209b
Gui-dev <adrianon10@gmail.com>
2022-07-19 03:16:32 -0300

    feat: add Boss and new animations

--- scripts/Boss.gd
+++ scripts/Boss.gd
@@ -3,12 +3,33 @@ extends EnemyBase
 
 func _ready() -> void:
   set_physics_process(false)
-   
+
+
+func _set_animation() -> void:
+  var animation = 'run'
+  
+  if $ray_wall.is_colliding():
+    animation = 'idle'
+  elif motion.x != 0 and health > 3:
+    animation = 'run'
+  elif motion.x != 0 and health < 3:
+    animation = 'angry_run'
+    speed = 64
+    
+  if hitted:
+    animation = 'hit'
+    
+  if health < 1:
+    animation = 'die'
+  
+  if $animation.assigned_animation != animation:
+    $animation.play(animation)
+       
 
 func _physics_process(delta: float) -> void:
   apply_gravity(delta) 
   _set_animation()
-
+  
 
 func _on_Trigger_player_entered() -> void:
- set_physics_process(true)
+  set_physics_process(true)
